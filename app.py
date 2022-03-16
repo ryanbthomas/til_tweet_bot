8f7d9d75980c9a763601eb94223ff1c4609dc9ba
Ryan Thomas <rthomas2003@gmail.com>
2022-03-15 22:26:26 -0400

    Updated app.py

--- app.py
+++ app.py
@@ -1,2 +1,15 @@
 import requests as reqs
+import os
+import pandas as pd
+import json
 
+url = "https://api.twitter.com/2/tweets/search/recent?query=from:ryebreadnyc&tweet.fields=created_at,geo"
+
+bearer_token = os.environ['TWITTER_BEARER_TOKEN']
+hed = {'Authorization': 'Bearer ' + bearer_token}
+response = reqs.get(url, headers = hed)
+
+data_dict = json.loads(response.content.decode('utf8'))
+data = pd.DataFrame(data_dict['data'])
+til_tweets = data[data['text'].str.startswith('TIL')]
+print(til_tweets)
